2c4c5c7bd99c867eda3b5b80455517440a4e76ca
mike <mikeqdev@users.noreply.github.com>
2025-01-13 22:22:04 -0500

    populate multiple employers at once

--- resume/sed.sh
+++ resume/sed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 6 ]; then
   echo "Usage: $0 <your name> <your email> <your phone number> <company name>"
   exit 1
 fi
 
-sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/5555555555/$3/ ; s/COMPANYNAME/$4/g" index.html
+sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/5555555555/$3/ ; 0,/COMPANYNAME/s//${4}/ ; 0,/COMPANYNAME/s//${5}/ ; 0,/COMPANYNAME/s//${6}/" index.html

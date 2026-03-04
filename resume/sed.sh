078939a39d35420d5f0b6076794898e3a99215df
mike <mikeqdev@users.noreply.github.com>
2026-03-04 17:06:17 -0500

    add platform eng

--- resume/sed.sh
+++ resume/sed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 6 ]; then
-  echo "Usage: $0 <your name> <your email> <your phone number> <company name>"
+if [ $# -ne 7 ]; then
+  echo "Usage: $0 <your name> <your email> <your phone number> <company name> <file name>"
   exit 1
 fi
 
-sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/5555555555/$3/ ; 0,/COMPANYNAME/s//${4}/ ; 0,/COMPANYNAME/s//${5}/ ; 0,/COMPANYNAME/s//${6}/" index.html
+sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/5555555555/$3/ ; 0,/COMPANYNAME/s//${4}/ ; 0,/COMPANYNAME/s//${5}/ ; 0,/COMPANYNAME/s//${6}/" "$7"

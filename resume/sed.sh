fe937a0e189238aa7ba502ea48372102b16b6c14
mike <mikeqdev@users.noreply.github.com>
2026-03-18 17:54:36 -0400

    overhaul: linkedin, mission stmt, bulletpoints

--- resume/sed.sh
+++ resume/sed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $# -ne 7 ]; then
-  echo "Usage: $0 <your name> <your email> <your phone number> <company name> <file name>"
+  echo "Usage: $0 <your name> <your email> <your linkedin user url> <company name> <file name>"
   exit 1
 fi
 
-sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/5555555555/$3/ ; 0,/COMPANYNAME/s//${4}/ ; 0,/COMPANYNAME/s//${5}/ ; 0,/COMPANYNAME/s//${6}/" "$7"
+sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/YOURLINKEDIN/$3/ ; 0,/COMPANYNAME/s//${4}/ ; 0,/COMPANYNAME/s//${5}/ ; 0,/COMPANYNAME/s//${6}/" "$7"
